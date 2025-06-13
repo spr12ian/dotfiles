76fc1b2c5fd4e5b3c296a841fcb7f278a3b935a4
spr12ian <173560+spr12ian@users.noreply.github.com>
2025-06-13 19:32:27 +0100

    Let's test again

--- q.sh
+++ q.sh
@@ -3,3 +3,5 @@ CALLER=$(grep "CALLER=" ~/.ssh/authorized_keys | awk '{print $NF}' | sed 's/^CAL
 sudo cp /home/$CALLER/projects/dotfiles/.bash_profile ./
 sudo cp /home/$CALLER/projects/dotfiles/.bashrc ./
 sudo cp /home/$CALLER/projects/dotfiles/.post_bashrc ./
+sudo chown $USER:$USER .bash_profile .bashrc .post_bashrc
+
