8b1c90f664c39e0e3be59aa4c19b14a5f2c516ef
spr12ian <173560+spr12ian@users.noreply.github.com>
2025-06-17 20:46:46 +0100

    remove junk

--- q.sh
+++
@@ -1,7 +0,0 @@
-CALLER=$(grep "CALLER=" ~/.ssh/authorized_keys | awk '{print $NF}' | sed 's/^CALLER=//')
-
-sudo cp /home/$CALLER/projects/dotfiles/.bash_profile ./
-sudo cp /home/$CALLER/projects/dotfiles/.bashrc ./
-sudo cp /home/$CALLER/projects/dotfiles/.post_bashrc ./
-sudo chown $USER:$USER .bash_profile .bashrc .post_bashrc
-
